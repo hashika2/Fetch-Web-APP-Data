7bba37d12bb6a95ea2538ce96b0679110168a1a0
hashika <m.g.hashikamaduranga@gmail.com>
2023-11-10 16:52:06 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
+import cheerio from 'cheerio';
+
 import './App.css';
 
 function App() {
+  const [webPageUrl, setWebPageUrl] = useState('');
+  const [title, setTitle] = useState('');
+  const [images, setImages] = useState([]);
+
+  const fetchWebPageDetails = async () => {
+    try {
+      const response = await axios.get(webPageUrl);
+      const html = response.data;
+      const $ = cheerio.load(html);
+
+      // Extract title
+      const pageTitle = $('title').text();
+      setTitle(pageTitle);
+
+      // Extract images
+      const pageImages = [];
+      $('img').each((index, element) => {
+        const imageUrl = $(element).attr('src');
+        pageImages.push(imageUrl);
+      });
+      setImages(pageImages);
+    } catch (error) {
+      console.error('Error fetching web page details:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Web Page Details</h1>
+      <div>
+        <input
+          type="text"
+          value={webPageUrl}
+          onChange={(e) => setWebPageUrl(e.target.value)}
+          placeholder="Enter a web page URL"
+        />
+        <button onClick={fetchWebPageDetails}>Fetch Details</button>
+      </div>
+      <div>
+        <h2>Title: {title}</h2>
+        <h2>Images:</h2>
+        <ul>
+          {images.map((image, index) => (
+            <li key={index}>
+              <img src={image} alt={`Image ${index}`} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
